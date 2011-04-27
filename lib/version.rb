97c3b7a01a5fbebc100bad4c50b0d40955778da3
Javier Juarez <javier.juarez@gmail.com>
2011-04-27 18:44:45 +0200

    Progress for the version information

--- lib/version.rb
+++ lib/version.rb
@@ -1,12 +1,10 @@
-module MiniLogger
-  module Version
-    INFO = {
-      :major =>0,
-      :minor =>4,
-      :patch =>2
-    }
+module Version
+  INFO = {
+    :major =>0,
+    :minor =>4,
+    :patch =>2
+  }
 
-    NAME   = 'mini_logger'
-    NUMBER = [INFO[:major], INFO[:minor], INFO[:patch]].values.join( '.' )
-  end
+  NAME   = 'mini_logger'
+  NUMBER = [INFO[:major], INFO[:minor], INFO[:patch]].join( '.' )
 end
